ef7f840654f8f332ae0e547146a9859105cbacc1
manuelvillanueva <50333780+manuelvillanueva@users.noreply.github.com>
2023-01-14 02:15:25 -0600

    ssr test

--- src/server-side/Client.tsx
+++ src/server-side/Client.tsx
@@ -1,6 +1,6 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+// import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-export const client = new ApolloClient({
-  uri: "https://api-us-east-1.hygraph.com/v2/cku8onzwd44mz01xnb6n16f2k/master",
-  cache: new InMemoryCache(),
-});
+// export const client = new ApolloClient({
+//   uri: "https://api-us-east-1.hygraph.com/v2/cku8onzwd44mz01xnb6n16f2k/master",
+//   cache: new InMemoryCache(),
+// });
