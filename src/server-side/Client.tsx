5fbc4cdeaaf0059e15e00371af73d072f840f106
manuelvillanueva <50333780+manuelvillanueva@users.noreply.github.com>
2023-01-15 16:51:45 -0600

    active ssr mode

--- src/server-side/Client.tsx
+++ src/server-side/Client.tsx
@@ -1,6 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+// import { HttpLink } from "apollo-link-http";
+// import { RetryLink } from "apollo-link-retry";
 
 export const client = new ApolloClient({
+  ssrMode: true,
+  // link: new HttpLink({
   uri: "https://api-us-east-1.hygraph.com/v2/cku8onzwd44mz01xnb6n16f2k/master",
+  // }).concat(new RetryLink()),
   cache: new InMemoryCache(),
 });
